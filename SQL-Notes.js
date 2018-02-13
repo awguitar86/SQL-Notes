dc5f273fed9c6ced1e3d22bf3e049da09922563e
Austin Wright <awguitar86@gmail.com>
2018-02-13 16:00:34 -0700

    wrote notes on keywords for conditional selects

--- SQL-Notes.js
+++ SQL-Notes.js
@@ -26,5 +26,30 @@ SELECT [column], [column] FROM [table];
 // Capitilaztion is an industry standard to make queries easier to read.
 // Seperate column names with a comma when selecting multiple.
 
-/* Where Keyword for Conditional Selects */
+/* WHERE Keyword for Conditional Selects */
+SELECT * FROM [table] WHERE [column] < [number];
+SELECT * FROM cities WHERE population < 1000000; 
+SELECT * FROM cities WHERE name = 'Chicago'; // a property in database has to be rapped in single quotes only. And properties are case sensitive.
+/* Logical Operators */
+//      >       Greater Than
+//      <       Less Than
+//      >=      Greater than or Equal to
+//      <=      Less than or Equal to
+//      =       Equals
+//   != or <>   Does Not Equal
+/* AND/OR operators */
+SELECT * FROM cities WHERE population > 1000000 AND name = 'San Diego' OR state_id = 5; //Evaluates WHERE and AND first then the OR
+/* LIKE operator */
+SELECT * FROM cities WHERE name LIKE 'chicago'; //use LIKE keyword instead of operator to do kind of a search where the property is not case sensitive.
+/* IS NULL and IS NOT NULL */
+SELECT * FROM cities WHERE name IS NULL;    //must use IS instead of a logical operator when using NULL or NOT NULL
+SELECT * FROM cities WHERE name IS NOT NULL; 
+/* BETWEEN */
+SELECT * FROM cities WHERE founded BETWEEN 1800 AND 1900; //finds the rows that are between those years in column founded
+/* IN/NOT IN */
+SELECT * FROM cities WHERE state_id IN (3, 4); //this is kind of like an array, where it will only return the rows in the table that have the state_id of 3 or 4 or whatever other numbers are in there. 
+SELECT * FROM cities WHERE state_id NOT IN (3, 4); //this returns all the rows in the table that do not have the numbers in the parenthesis.
+
+/* Adding, Updating, and Removing */
+
 
