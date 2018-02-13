84d35b2ba62b0b743ee567d31b9b5b406fdd5f9d
Austin Wright <awguitar86@gmail.com>
2018-02-13 12:01:10 -0700

    first commit adding files and folders to repo

---
+++ SQL-Notes.js
@@ -0,0 +1,6 @@
+/*** SQL ***/
+
+/* REALATIONAL DATABASE */
+// relational database: a databse which is structured to show the relationships between data. They are like giant connected excel spreadsheets.
+// Uses tables with columns and rows which relate to one another.
+
