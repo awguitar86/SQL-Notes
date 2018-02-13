5c19bd5532080fad00c94c6647e0e456d717d0ee
Austin Wright <awguitar86@gmail.com>
2018-02-13 12:10:16 -0700

    more notes on primary keys

--- SQL-Notes.js
+++ SQL-Notes.js
@@ -8,4 +8,6 @@
 // Any one column assigned to identity each row; must be unique and constant.
 // Using an ID column is a very common solution - makes it easy to connecy with other tables.
 // The email or username can also be used as the primary key, but there can be problems with that. 
+// Using an ID as the primary key allows other columns to be the same, while having a unique key for each item.
+
 
