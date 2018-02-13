18a810594491eae83465000d25ecdb14f8582737
Austin Wright <awguitar86@gmail.com>
2018-02-13 12:08:29 -0700

    added notes on primary keys

--- SQL-Notes.js
+++ SQL-Notes.js
@@ -5,4 +5,7 @@
 // Uses tables with columns and rows which relate to one another.
 
 /* Primary Keys */
-// 
+// Any one column assigned to identity each row; must be unique and constant.
+// Using an ID column is a very common solution - makes it easy to connecy with other tables.
+// The email or username can also be used as the primary key, but there can be problems with that. 
+
