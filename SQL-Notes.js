be93e97b04046f83d7b43f9bf9340686dfbcdedd
Austin Wright <awguitar86@gmail.com>
2018-02-13 12:03:07 -0700

    changes

--- SQL-Notes.js
+++ SQL-Notes.js
@@ -1,6 +1,8 @@
 /*** SQL ***/
 
-/* REALATIONAL DATABASE */
-// relational database: a databse which is structured to show the relationships between data. They are like giant connected excel spreadsheets.
+/* Relational Database */
+// A database which is structured to show the relationships between data. They are like giant connected excel spreadsheets.
 // Uses tables with columns and rows which relate to one another.
 
+/* Primary Keys */
+// 
