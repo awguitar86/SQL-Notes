2fff3a9e96d466aca65d8fa02824d19b9b4e2a38
Austin Wright <awguitar86@gmail.com>
2018-02-14 16:57:30 -0700

    changes

--- SQL-Notes.sql
+++ SQL-Notes.sql
@@ -6,7 +6,7 @@
 
 /* Primary Keys */
 -- Any one column assigned to identity each row; must be unique and constant.
--- Using an ID column is a very common solution - makes it easy to connecy with other tables.
+-- Using an ID column is a very common solution - makes it easy to connect with other tables.
 -- The email or username can also be used as the primary key, but there can be problems with that. 
 -- Using an ID as the primary key allows other columns to be the same, while having a unique key for each item.
 
@@ -15,7 +15,7 @@
 -- The layout of a table; includes column names, data types, and modifiers (like primary key and foreign keys).
 
 /* Select Statments */
--- Are our primary mecanism to help us pull information out of databases. 
+-- Are our primary mechanism to help us pull information out of databases. 
 -- Another term for select statments is query. Always terminate statments with semi-colons.
 
 SELECT * FROM [table]; -- !!be sure to end each query with a semicolon!!
